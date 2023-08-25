1043aa0fa530a125404accc254fd4d61264afb26
Evgeniya <evgeniya@ultimatemovers.net>
2023-08-25 18:22:27 +0200

    added font

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -1,6 +1,7 @@
 ko.components.register("accordion", {
   viewModel: function (params) {
-    this.isOpen = ko.observable(false);
+    this.isOpen = ko.observable(false); // Start with the accordion closed
+
     this.toggleAccordion = function () {
       this.isOpen(!this.isOpen());
     };
@@ -10,13 +11,14 @@ ko.components.register("accordion", {
   },
   template: `
     <div>
-      <div class="accordion">
-        <div class="accordion__label" data-bind="click:toggleAccordion">
-          <h2 data-bind="text:title"></h2>
+      <div class="accordion" data-bind="css: { opened: isOpen }">
+        <div class="accordion__label" data-bind="click: toggleAccordion">
+          <h2 data-bind="text: title"></h2>
         </div>
-        <div class="accordion__options"  data-bind="if: isOpen">
-          <div data-bind="foreach:options">
-          <div data-bind="text: label"></div></div>
+        <div class="accordion__options" data-bind="css: { open: isOpen }">
+          <div data-bind="foreach: options">
+            <div  class="option"  data-bind="text: label"></div>
+          </div>
         </div>
       </div>
     </div>
