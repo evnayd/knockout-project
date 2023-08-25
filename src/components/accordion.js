ea105ebda0062bf1608a06146b88fb218337a851
Evgeniya <evgeniya@ultimatemovers.net>
2023-08-25 15:21:09 +0200

    created project structure

---
+++ src/components/accordion.js
@@ -0,0 +1,12 @@
+ko.components.register("accordion", {
+  viewModel: function (params) {
+    console.log("hello, im accordion");
+  },
+  template: `
+      <div>
+        <h2>hello, i'm accordion</h2>
+     
+  
+      </div>
+    `,
+});
---
+++ src/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Your Knockout.js code here
+
+  // Define your Knockout.js view model and apply bindings
+  function AppViewModel() {
+    // ...
+  }
+
+  var viewModel = new AppViewModel();
+  ko.applyBindings(viewModel);
+});
